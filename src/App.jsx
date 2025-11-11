4fb4ba0e48492641083de1c9be4f76e02d303c3d
stan10aviles <stan10aviles@hotmail.com>
2025-11-11 17:29:15 -0500

    Created member, and feedback search pages. Added logic to handle DTos. will need to further test and clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
 import './App.css'
-import Survey from './pages/Survey'
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import Welcome from './pages/Welcome';
+import SubmitFeedback from './pages/SubmitFeedback';
+import MemberIdSearch from './pages/MemberIdSearch';
+import FeedbackIdSearch from './pages/FeedbackIdSearch';
 
 function App() {
 
   return (
     <>
-    <Survey />
+    <BrowserRouter>
+    <nav>
+      <Link to="/">Home</Link> |
+      <Link to="/submit"> Submit Feedback</Link> |
+      <Link to="/memidsearch"> Member ID Search</Link> |
+      <Link to="/feedbackidsearch"> Feedback ID Search</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Welcome />}></Route>
+      <Route path="/submit" element={<SubmitFeedback />}></Route>
+      <Route path="/memidsearch" element={<MemberIdSearch />}></Route>
+      <Route path="/feedbackidsearch" element={<FeedbackIdSearch />}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,10 @@
+function Welcome() {
+    return (
+        <>
+        <h2>Member Feedback Dashboard</h2>
+         <p> Submit and access your feedback submissions, all in one place!</p>
+        </>
+    );
+}
+
+export default Welcome;
